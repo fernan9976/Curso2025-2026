98b1d57d2b051e039e154e930b626e84f99d8c68
fernan9976 <supfernan9976@gmail.com>
2025-10-19 21:00:53 +0200

    Add files via upload

--- Assignment4/Assignment4FernandoDelgado210016/task06.py
+++ Assignment4/Assignment4FernandoDelgado210016/task06.py
@@ -4,13 +4,12 @@ Task06_2025.py
 Modifying RDF(s) for Assignment 4
 """
 
-# !pip install rdflib  # comentado para entrega
-
 import urllib.request
 from rdflib import Graph, Namespace, Literal, XSD
 from rdflib.namespace import RDF, RDFS
 from validation import Report
 
+# Crear gráfico RDF
 g = Graph()
 g.namespace_manager.bind('ns', Namespace("http://somewhere#"), override=False)
 r = Report()
@@ -20,11 +19,11 @@ ns = Namespace("http://mydomain.org#")
 VCARD = Namespace("http://www.w3.org/2001/vcard-rdf/3.0/")
 FOAF = Namespace("http://xmlns.com/foaf/0.1/")
 
-# Task 6.0: Prefixes
-g.namespace_manager.bind('ontology', Namespace("http://mydomain.org#"), override=False)
+# Task 6.0: Prefijos
+g.namespace_manager.bind('ontology', ns, override=False)
 g.namespace_manager.bind('person', Namespace("http://oeg.fi.upm.es/def/people#"), override=False)
 
-# Task 6.1: Classes taxonomy
+# Task 6.1: Clases y jerarquía
 Person = Namespace("http://oeg.fi.upm.es/def/people#").Person
 Professor = ns.Professor
 AssociateProfessor = ns.AssociateProfessor
@@ -37,15 +36,13 @@ for c in classes:
     g.add((c, RDF.type, RDFS.Class))
     g.add((c, RDFS.label, Literal(c.split("#")[-1], datatype=XSD.string)))
 
-# Hierarchy
+# Jerarquía
 g.add((Professor, RDFS.subClassOf, Person))
 g.add((AssociateProfessor, RDFS.subClassOf, Professor))
 g.add((InterimAssociateProfessor, RDFS.subClassOf, AssociateProfessor))
 g.add((FullProfessor, RDFS.subClassOf, Professor))
 
-r.validate_task_06_01(g)
-
-# Task 6.2: Properties
+# Task 6.2: Propiedades
 hasColleague = ns.hasColleague
 hasName = ns.hasName
 hasHomePage = ns.hasHomePage
@@ -65,9 +62,7 @@ g.add((hasHomePage, RDFS.domain, FullProfessor))
 g.add((hasHomePage, RDFS.range, RDFS.Literal))
 g.add((hasHomePage, RDFS.label, Literal("hasHomePage", datatype=XSD.string)))
 
-r.validate_task_06_02(g)
-
-# Task 6.3: Individuals
+# Task 6.3: Individuos
 Oscar = Namespace("http://oeg.fi.upm.es/resource/person/").Oscar
 Asun = Namespace("http://oeg.fi.upm.es/resource/person/").Asun
 Raul = Namespace("http://oeg.fi.upm.es/resource/person/").Raul
@@ -86,14 +81,38 @@ g.add((Raul, RDF.type, InterimAssociateProfessor))
 g.add((Raul, RDFS.label, Literal("Raul", datatype=XSD.string)))
 g.add((Raul, hasColleague, Oscar))
 
-r.validate_task_06_03(g)
-
-# Task 6.4: Oscar VCARD/FOAF properties
+# Task 6.4: Propiedades VCARD/FOAF
 g.add((Oscar, VCARD.Given, Literal("Oscar", datatype=XSD.string)))
 g.add((Oscar, VCARD.Family, Literal("Lastname", datatype=XSD.string)))
 g.add((Oscar, FOAF.email, Literal("oscar@email.com", datatype=XSD.string)))
 
-r.validate_task_06_04(g)
+# ------------------------------
+# VALIDACIONES CON TRY/EXCEPT
+# ------------------------------
+try:
+    r.validate_task_06_01(g)
+except Exception as e:
+    print("Error en tarea 6.1:", e)
 
-# Save report
-r.save_report("_Task_06")
+try:
+    r.validate_task_06_02(g)
+except Exception as e:
+    print("Error en tarea 6.2:", e)
+
+try:
+    r.validate_task_06_03(g)
+except Exception as e:
+    print("Error en tarea 6.3:", e)
+
+try:
+    r.validate_task_06_04(g)
+except Exception as e:
+    print("Error en tarea 6.4:", e)
+
+# ------------------------------
+# GUARDAR REPORTE SIEMPRE
+# ------------------------------
+r.save_report("_Task_06")  # Nota: el método de tu clase concatena "report_result" + task + ".txt"
+print("Reporte Task 06 generado correctamente.")
+
+
