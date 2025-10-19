990fe94a5f522fa643ed0682f8d6a86651ae8ac4
fernan9976 <supfernan9976@gmail.com>
2025-10-19 20:51:23 +0200

    Add files via upload

--- Assignment4/Assignment4FernandoDelgado210016/task07.py
+++ Assignment4/Assignment4FernandoDelgado210016/task07.py
@@ -1,151 +1,125 @@
-#!pip install rdflib
 # -*- coding: utf-8 -*-
-
-
-import urllib.request
-url = 'https://raw.githubusercontent.com/FacultadInformatica-LinkedData/Curso2025-2026/refs/heads/master/Assignment4/course_materials/python/validation.py'
-urllib.request.urlretrieve(url, 'validation.py')
-source_repo = "https://raw.githubusercontent.com/FacultadInformatica-LinkedData/Curso2025-2026/master/Assignment4/course_materials"
-
-from validation import Report
-from rdflib import Graph, Namespace
-from rdflib.namespace import RDF, RDFS
-from rdflib.plugins.sparql import prepareQuery
-
-# Carga del grafo RDF
-rdf_graph = Graph()
-ns = Namespace("http://somewhere#")
-rdf_graph.namespace_manager.bind("ns", ns, override=False)
-rdf_graph.parse(source_repo + "/rdf/data06.ttl", format="turtle")
-
-rep = Report()
-
-# ==========================================================
-# TASK 7.1a - RDFLib: clases y superclases
-# ==========================================================
-resultado = []
-vistos = set()
-
-for clase in rdf_graph.subjects(RDF.type, RDFS.Class):
-    if clase in vistos:
-        continue
-    vistos.add(clase)
-    super_clase = rdf_graph.value(subject=clase, predicate=RDFS.subClassOf)
-    resultado.append((clase, super_clase))
-
-for par in resultado:
-    print(f"{par[0]} --> {par[1]}")
-
-rep.validate_07_1a(resultado)
-
-# ==========================================================
-# TASK 7.1b - SPARQL
-# ==========================================================
-# ==========================================================
-# TASK 7.1b - SPARQL
-# ==========================================================
-consulta_1b = """
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-
-SELECT DISTINCT ?c ?sc
-WHERE {
-  ?c rdf:type rdfs:Class .
-  OPTIONAL { ?c rdfs:subClassOf ?sc . }
-}
+"""
+Task07_2025.py
+Querying RDF(s) for Assignment 4
 """
 
-for fila in rdf_graph.query(consulta_1b):
-    print(fila.c, fila.sc)
+from rdflib import Graph, Namespace, RDF, RDFS, Literal
+from rdflib.plugins.sparql import prepareQuery
+from validation import Report
 
-rep.validate_07_1b(consulta_1b, rdf_graph)
+# Crear el grafo
+g = Graph()
+g.parse("data06.ttl", format="ttl")  # asegúrate de guardar tu task06 en TTL
 
+report = Report()
 
-# ==========================================================
-# TASK 7.2a - RDFLib: individuos de Person
-# ==========================================================
-p = Namespace("http://oeg.fi.upm.es/def/people#")
+# Namespaces
+ns = Namespace("http://mydomain.org#")          # Profesores y propiedades custom
+p = Namespace("http://oeg.fi.upm.es/def/people#")  # Persona
 
-todas = set(rdf_graph.transitive_subjects(RDFS.subClassOf, p.Person)) | {p.Person}
-individuos = sorted({ind for cls in todas for ind in rdf_graph.subjects(RDF.type, cls)}, key=str)
+# -----------------------------
+# TASK 7.1a: List classes + superclasses (RDFLib)
+# -----------------------------
+result = []
+vistas = set()
+for c in g.subjects(RDF.type, RDFS.Class):
+    if c in vistas:
+        continue
+    vistas.add(c)
+    sc = g.value(subject=c, predicate=RDFS.subClassOf, object=None)  # None si no tiene superclase
+    result.append((c, sc))
 
-for i in individuos:
+for r in result:
+    print(r)
+
+report.validate_07_1a(result)
+
+# -----------------------------
+# TASK 7.1b: Same with SPARQL
+# -----------------------------
+query_71b = prepareQuery('''
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT DISTINCT ?c ?sc WHERE {
+    ?c a rdfs:Class .
+    OPTIONAL { ?c rdfs:subClassOf ?sc }
+}
+''')
+
+for r in g.query(query_71b):
+    print(r.c, r.sc)
+
+report.validate_07_1b(query_71b, g)
+
+# -----------------------------
+# TASK 7.2a: List all individuals of Person + subclasses (RDFLib)
+# -----------------------------
+classes_person = set(g.transitive_subjects(RDFS.subClassOf, p.Person)) | {p.Person}
+individuals = sorted({s for c in classes_person for s in g.subjects(RDF.type, c)}, key=str)
+
+for i in individuals:
     print(i)
 
-rep.validate_07_02a(individuos)
+report.validate_07_02a(individuals)
 
-# ==========================================================
-# TASK 7.2b - SPARQL
-# ==========================================================
-# TASK 7.2b
-from rdflib import Namespace
-
-PEOPLE = Namespace("http://oeg.fi.upm.es/def/people#")
-
-consulta_2b = """
-PREFIX people: <http://oeg.fi.upm.es/def/people#>
-
-SELECT ?ind
-WHERE {
-    ?ind a people:FullProfessor .
-}
-"""
-
-rep.validate_07_02b(rdf_graph, consulta_2b)
-
-
-# ==========================================================
-# TASK 7.3 - SPARQL: quienes conocen a Rocky
-# ==========================================================
-consulta_3 = prepareQuery("""
+# -----------------------------
+# TASK 7.2b: Same with SPARQL
+# -----------------------------
+query_72b = prepareQuery('''
 PREFIX p: <http://oeg.fi.upm.es/def/people#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-
-SELECT ?nombre ?tipo
-WHERE {
-  ?ind p:knows p:Rocky ;
-       rdf:type ?tipo .
-  {
-    ?ind p:hasName ?nombre .
-  }
-  UNION
-  {
-    ?ind rdfs:label ?nombre .
-  }
-}
-""")
-
-for fila in rdf_graph.query(consulta_3):
-    print(fila.nombre, fila.tipo)
-
-rep.validate_07_03(rdf_graph, consulta_3)
-
-# ==========================================================
-# TASK 7.4 - SPARQL: entidades con colega con perro
-# ==========================================================
-consulta_4 = prepareQuery("""
-PREFIX p: <http://oeg.fi.upm.es/def/people#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-
-SELECT DISTINCT ?nombre
-WHERE {
-  ?e rdfs:label ?nombre ;
-     p:hasColleague ?col .
-  
-  {
-    ?col p:ownsPet ?m .
-  }
-  UNION
-  {
-    ?col p:hasColleague ?col2 .
-    ?col2 p:ownsPet ?m .
-  }
+SELECT DISTINCT ?ind WHERE {
+    ?ind rdf:type ?s .
+    ?s rdfs:subClassOf* p:Person .
 }
-""")
+''')
 
-for x in rdf_graph.query(consulta_4):
-    print(x.nombre)
+for r in g.query(query_72b):
+    print(r.ind)
 
-rep.validate_07_04(rdf_graph, consulta_4)
-rep.save_report("_Task_07")
+report.validate_07_02b(g, query_72b)
+
+# -----------------------------
+# TASK 7.3: Name and type of those who know Rocky (SPARQL)
+# -----------------------------
+query_73 = prepareQuery('''
+PREFIX p: <http://mydomain.org#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT ?name ?type WHERE {
+    ?ind p:knows p:Rocky .
+    ?ind rdf:type ?type .
+    { ?ind p:hasName ?name }
+    UNION { ?ind rdfs:label ?name }
+}
+''')
+
+for r in g.query(query_73):
+    print(r.name, r.type)
+
+report.validate_07_03(g, query_73)
+
+# -----------------------------
+# TASK 7.4: Entities with colleague with dog (SPARQL)
+# -----------------------------
+query_74 = prepareQuery('''
+PREFIX p: <http://mydomain.org#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+SELECT DISTINCT ?name WHERE {
+    ?ind rdfs:label ?name .
+    ?ind p:hasColleague ?c1 .
+    { ?c1 p:ownsPet ?pet }
+    UNION {
+        ?c1 p:hasColleague ?c2 .
+        ?c2 p:ownsPet ?pet
+    }
+}
+''')
+
+for r in g.query(query_74):
+    print(r.name)
+
+report.validate_07_04(g, query_74)
+report.save_report("_Task_07")
+
